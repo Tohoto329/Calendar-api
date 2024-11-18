8dbbf23314d872bc4ca81d68b41a6b63a7fd9956
Tohoto329 <tohotochophy329@gmail.com>
2024-11-19 03:17:57 +0530

    log changes

--- config/logger.js
+++ config/logger.js
@@ -1,15 +1,18 @@
 const winston = require('winston');
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 const logger = winston.createLogger({
     level: 'info',
     format: winston.format.combine(
-        winston.format.timestamp(), 
+        winston.format.timestamp(),
         winston.format.simple()
     ),
     transports: [
-        new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+
+        !isProduction && new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
         new winston.transports.Console({ format: winston.format.simple() })
-    ],
+    ].filter(Boolean),
 });
 
 module.exports = logger;
